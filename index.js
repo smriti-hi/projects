b951407a9e461f33b4a9ae36637e419e6562cf63
Jeshika-code <jeshikasapkota5@gmail.com>
2024-04-26 09:14:03 +0545

    jquery added

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ $(document).ready(function () {
           }
         }
       });
-  
+      // var currentPage = window.location.pathname.split('/').pop();
+      
       if (firstEmptyField) {
         alert("Please fill in all fields!");
         firstEmptyField.focus();
       } 
-      else {
+      else  {
         window.location.href = "educational.html";
       }
     });
   
-    $("#first_name, #middle_name, #last_name, #city_name, #country, #collegename,#stream").on(
+    $("#first_name, #middle_name, #last_name, #city_name, #country, #collegename,#stream,#guardian-name,#profession,#counselled").on(
       "input",
       function () {
         var inputValue = $(this).val();
@@ -41,7 +42,7 @@ $(document).ready(function () {
         }
       }
     );
-  
+
     $("#email").on("blur", function () {
       var emailValue = $(this).val();
       var emailPattern = /^[A-Za-z][A-Za-z0-9._-]*@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
@@ -59,7 +60,7 @@ $(document).ready(function () {
       }
   });
   $('#passout_year').datepicker({
-    changeMonth:false,
+    // changeMonth:false,
     changeYear:true,
     showButtonPanel:true,
     dateFormat:'yy',
@@ -68,5 +69,79 @@ $(document).ready(function () {
         $(this).datepicker('setDate', new Date(year, 1));
     }
   })
+
+  $('#passout_year').focus(function () {
+    $('.ui-datepicker-calendar').hide();
+    $('.ui-datepicker-month').hide();
+    $('.ui-datepicker-prev, .ui-datepicker-next').hide();
+    $('.ui-datepicker-current').hide();
+    $('#ui-datepicker-div').position({
+        my: 'center top',
+        at: 'center bottom',
+        of: $(this)
+    });
+});
+$(".grade-select").change(function () {
+  var selectedOption = $(this).val();
+  var parentDiv = $(this).closest('.input-row');
+  
+  // Remove any existing input fields next to the dropdown
+  parentDiv.find('.additional-input').remove();
+
+  // Add input field based on selected option
+  if (selectedOption === "GPA") {
+    var inputField = $('<input type="text" class="additional-input" placeholder="Enter ' + selectedOption + '">');
+    inputField.css({
+        'margin-top': '-20px',
+        'margin-left': '15px',
+        'width': '190px',
+        'padding': '5px',
+        // 'border': '1px solid #ccc',
+        'border-radius': '5px',
+        'box-sizing': 'border-box'
+    });
+    inputField.on("input", function () {
+      var gpa = $(this).val();
+
+      if (gpa === "") {
+          return; 
+      }
+      if (isNaN(parseFloat(gpa)) || parseFloat(gpa) < 0 || parseFloat(gpa) > 4.0) {
+          alert("Please enter a valid GPA.");
+          $(this).val("");
+          return;
+      }
+      if (gpa.length === 2 && gpa.charAt(1) !== ".") {
+          alert("Please enter a valid GPA.");
+          $(this).val(gpa.charAt(0)); 
+      }
+  });
+    parentDiv.append(inputField);
+}
+else   if (selectedOption === "Percentage") {
+  var inputField = $('<input type="text" class="additional-input" placeholder="Enter ' + selectedOption + '">');
+  inputField.css({
+      'margin-top': '-20px',
+      'margin-left': '15px',
+      'width': '190px',
+      'padding': '5px',
+      // 'border': '1px solid #ccc',
+      'border-radius': '5px',
+      'box-sizing': 'border-box'
+  });
+  inputField.on("input", function () {
+    var inputValue = $(this).val();
+    // Regular expression to match percentage format (0-100 with optional decimal)
+    var percentagePattern = /^(100(?:\.0{1,2})?|\d{0,2}(?:\.\d{0,2})?)%?$/;
+
+    if (!percentagePattern.test(inputValue)) {
+     alert("invalid percentage")
+     $(this).val("");
+    } 
+});
+  parentDiv.append(inputField);
+}
+});
+
   });
   
